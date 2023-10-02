1f35f70bdcc428795bf4fef8f518c3f68fb188ea
shiv gaurav <shivgaurav27@gmail.com>
2023-10-02 16:00:00 +0530

    loading

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -16,11 +16,16 @@ const TodoList = () => {
 			.then((res) => res.data);
 	};
 
-	const { data: todos, error } = useQuery<Todo[], Error>({
+	const {
+		data: todos,
+		error,
+		isLoading,
+	} = useQuery<Todo[], Error>({
 		queryKey: ['todos'],
 		queryFn: fetchTodos,
 	});
 
+	if (isLoading) return <p>Loading...</p>;
 	if (error) return <p>{error.message}</p>;
 
 	return (
